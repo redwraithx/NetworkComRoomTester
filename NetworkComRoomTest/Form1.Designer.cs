99343844b088b917aa4503a233ef908dc1d608b9
Brian P <brianpurdy01@gmail.com>
2018-09-10 23:02:38 -0300

    this is something to use till we get our Power Supply Units networked and sorted out at work. just a simple utility to save me time in command shell pinging.

---
+++ NetworkComRoomTest/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace NetworkComRoomTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnScanAll = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.columnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnRoomNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnIP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PingTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnScanAll);
+            this.groupBox1.Controls.Add(this.dataGridView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(341, 560);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 537);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Brian P.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 532);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "By:";
+            // 
+            // btnScanAll
+            // 
+            this.btnScanAll.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnScanAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScanAll.Location = new System.Drawing.Point(132, 524);
+            this.btnScanAll.Name = "btnScanAll";
+            this.btnScanAll.Size = new System.Drawing.Size(75, 30);
+            this.btnScanAll.TabIndex = 1;
+            this.btnScanAll.Text = "Scan All";
+            this.btnScanAll.UseVisualStyleBackColor = false;
+            this.btnScanAll.Click += new System.EventHandler(this.btnScanAll_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnID,
+            this.columnRoomNumber,
+            this.columnIP,
+            this.columnStatus,
+            this.PingTime});
+            this.dataGridView.Enabled = false;
+            this.dataGridView.Location = new System.Drawing.Point(6, 9);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.ColumnHeaderSelect;
+            this.dataGridView.Size = new System.Drawing.Size(329, 509);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // columnID
+            // 
+            this.columnID.HeaderText = "ID";
+            this.columnID.Name = "columnID";
+            this.columnID.ReadOnly = true;
+            this.columnID.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.columnID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            this.columnID.Width = 35;
+            // 
+            // columnRoomNumber
+            // 
+            this.columnRoomNumber.HeaderText = "Room #";
+            this.columnRoomNumber.Name = "columnRoomNumber";
+            this.columnRoomNumber.ReadOnly = true;
+            this.columnRoomNumber.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.columnRoomNumber.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            this.columnRoomNumber.Width = 45;
+            // 
+            // columnIP
+            // 
+            this.columnIP.HeaderText = "IP Address";
+            this.columnIP.Name = "columnIP";
+            this.columnIP.ReadOnly = true;
+            this.columnIP.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.columnIP.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // columnStatus
+            // 
+            this.columnStatus.HeaderText = "Online Status";
+            this.columnStatus.Name = "columnStatus";
+            this.columnStatus.ReadOnly = true;
+            this.columnStatus.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.columnStatus.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // PingTime
+            // 
+            this.PingTime.HeaderText = "Ping (ms)";
+            this.PingTime.Name = "PingTime";
+            this.PingTime.ReadOnly = true;
+            this.PingTime.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.PingTime.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            this.PingTime.Width = 45;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 578);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comunication Room Status";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnScanAll;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnRoomNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnIP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PingTime;
+    }
+}
+
---
+++ NetworkComRoomTest/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+
+
+namespace NetworkComRoomTest
+{
+    public enum statusResult
+    {
+        UnChecked,
+        Online,
+        Offline,
+        TimedOut
+    }
+
+
+
+
+
+    public partial class Form1 : Form
+    {
+        private List<ScanData> dataList = new List<ScanData>();
+        private ScanData scanData;
+
+        // keep a index while iterating through the list, else its -1 (null in my app)
+        private int currentCell = -1;
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // status text alignment
+            this.dataGridView.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            this.dataGridView.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            this.dataGridView.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // set rows font / style
+            this.dataGridView.RowHeadersDefaultCellStyle.Font = new Font("Aria", 8F, FontStyle.Bold, GraphicsUnit.Pixel);
+
+            // set rows default color
+            this.dataGridView.RowsDefaultCellStyle.BackColor = Color.LightBlue;
+
+            // align and set font / style for the column headers
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                column.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                column.HeaderCell.Style.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Pixel);
+            }
+
+
+
+
+            #region AddRoomsToList
+
+
+            // build ip list (HARD CODED FOR NOW) as the follow order: ID, ROOM #, IP Address, Status, Ping Response
+            scanData = new ScanData("1", "P107", "10.11.0.7", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("2", "P110", "10.11.0.10", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("3", "P134", "10.11.0.34", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("4", "P155", "10.11.0.55", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("5", "120", "10.11.1.20", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("6", "138", "10.11.1.38", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("7", "200", "10.11.2.0", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("8", "233", "10.11.2.33", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("9", "280", "10.11.2.80", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("10", "329", "10.11.3.29", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("11", "363", "10.11.3.63", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("12", "380", "10.11.3.80", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("13", "400", "10.11.4.0", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("14", "431", "10.11.4.31", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("15", "461", "10.11.4.61", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("16", "490", "10.11.4.90", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("17", "506", "10.11.5.6", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("18", "539", "10.11.5.39", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("19", "564", "10.11.5.64", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("20", "600", "10.11.6.0", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("21", "634", "10.11.6.34", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+            scanData = new ScanData("22", "671", "10.11.6.71", statusResult.UnChecked.ToString(), "-1");
+            dataList.Add(scanData);
+
+
+            #endregion // end AddRoomsToList
+
+
+
+
+            foreach (ScanData item in dataList)
+            {
+                dataGridView.Rows.Add(item.ID, item.RoomNumber, item.IPAddress, item.Status, item.PingDelay);
+            }
+
+
+
+            SelectClear();
+        }
+
+
+
+
+
+
+        private void btnScanAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int timeout = 400;
+                Ping ping;
+            
+                foreach (ScanData item in dataList)
+                {
+                    // iter through list ping and report
+                    ping = new Ping();
+
+                    PingReply pingReply = ping.Send(item.IPAddress, timeout);
+
+                    SuspendLayout();
+                    SetStatus(item, pingReply);
+
+                    ResumeLayout();
+
+                    dataGridView.Refresh();
+                }
+
+                dataGridView.Refresh();
+            }
+            catch (PingException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);                
+            }
+        }
+
+
+
+
+
+        private void SetPingResponse(ScanData item, PingReply pingReply)//, Ping ping)
+        {
+            if (pingReply.Status == IPStatus.Success)
+            {
+                
+
+                currentCell = int.Parse(item.ID);
+                item.PingDelay = Convert.ToString(pingReply.RoundtripTime);
+
+                dataGridView.CurrentCell = dataGridView.Rows[currentCell - 1].Cells[dataGridView.ColumnCount - 1];
+
+                DataGridViewCell cell = dataGridView.Rows[currentCell - 1].Cells[4];
+                cell.Value = item.PingDelay;
+                
+            }
+            else
+            {
+                dataGridView.CurrentCell = dataGridView.Rows[currentCell - 1].Cells[dataGridView.ColumnCount - 1];
+
+                DataGridViewCell cell = dataGridView.Rows[currentCell - 1].Cells[4];
+                cell.Value = "999";
+            }
+
+        }
+
+
+
+
+        private void SetStatus(ScanData item, PingReply pingReply)
+        {
+            
+
+            if (pingReply.Status == IPStatus.Success)
+            {
+                currentCell = int.Parse(item.ID);
+
+                // online
+                dataGridView.CurrentCell = dataGridView.Rows[currentCell - 1].Cells[dataGridView.ColumnCount - 2];                              
+                
+                dataGridView.CurrentCell.Value = statusResult.Online;
+
+                DataGridViewCell cell = dataGridView.Rows[currentCell - 1].Cells[3];
+                cell.Style.BackColor = Color.Green;
+
+
+                SetPingResponse(item, pingReply);
+            }
+            else if (pingReply.Status == IPStatus.TimedOut)
+            {
+                currentCell = int.Parse(item.ID);
+                
+                dataGridView.CurrentCell = dataGridView.Rows[currentCell - 1].Cells[dataGridView.ColumnCount - 2];
+                
+                dataGridView.CurrentCell.Value = statusResult.Offline;
+
+                DataGridViewCell cell = dataGridView.Rows[currentCell - 1].Cells[3];
+                cell.Style.BackColor = Color.Red;
+
+
+                SetPingResponse(item, pingReply);
+            }            
+            else
+            {
+                currentCell = int.Parse(item.ID);
+
+                DataGridViewCell cell = dataGridView.Rows[currentCell - 1].Cells[3];
+                cell.Style.BackColor = Color.White;
+                                     
+                dataGridView.CurrentCell = dataGridView.Rows[currentCell - 1].Cells[dataGridView.ColumnCount - 2];
+                
+                                
+                currentCell = -1;
+
+
+                SetPingResponse(item, pingReply);
+            }
+
+            
+            SelectClear();
+        }
+
+
+
+
+
+        private void SelectClear()
+        {
+            dataGridView[0, 0].Selected = true;
+            dataGridView.ClearSelection();
+        }
+    }
+
+
+
+
+
+    class ScanData
+    {
+        public string ID { get; set; }
+        public string RoomNumber { get; set; }
+        public string IPAddress { get; set; }
+        public string Status { get; set; }
+        public string PingDelay { get; set; }
+
+        public ScanData()
+        {
+
+        }
+
+
+
+
+
+        public ScanData(string id, string roomNumber, string ipAddress, string status, string pingDelay)
+        {
+            ID = id;
+            RoomNumber = roomNumber;
+            IPAddress = ipAddress;
+            Status = status;
+            PingDelay = pingDelay;
+        }
+    }
+}
